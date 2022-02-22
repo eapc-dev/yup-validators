22113c7066729a7cdc6e01c99e3b3f639c1b614f
Jordan Monier <pro.jordan.monier@gmail.com>
2022-02-22 14:56:01 +0100

    feat: added webpack compilation

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+
+  output: {
+    filename: "index.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  mode: "development",
+  optimization: {
+    usedExports: true,
+    innerGraph: true,
+    sideEffects: true,
+  },
+  devtool: false,
+};
