8245ebc43497f5b8a9e5fac00b814f3f6bb55ce4
Jordan Monier <pro.jordan.monier@gmail.com>
2022-05-30 10:51:43 +0200

    feat: added date timezone on transformers

---
+++ src/schema/date/transformers/subtract.ts
@@ -0,0 +1,53 @@
+import dayjs from 'dayjs'
+import pluginTimezone from 'dayjs/plugin/timezone'
+import pluginUTC from 'dayjs/plugin/utc'
+
+import { IDateProps, TDateValidatorResult } from '../_types'
+
+dayjs.extend(pluginUTC)
+dayjs.extend(pluginTimezone)
+
+export interface ISubtractProps {
+  /**
+   * Unit to be used.
+   */
+  values: [unit: dayjs.ManipulateType, value: number][]
+
+  /**
+   * Timezone to be used.
+   */
+  timezone?: [tz: string, keepLocalTimes?: boolean]
+}
+
+/**
+ * Subtract values to the date using `dayjs`.
+ */
+export const subtract = (
+  props: ISubtractProps & Omit<IDateProps, 'message'>
+): TDateValidatorResult => {
+  const { values, timezone, active = true } = props ?? {}
+
+  return (schema) => {
+    if (active) {
+      schema = schema.transform((v) => {
+        let date = dayjs(v)
+
+        if (!date.isValid()) {
+          return v as unknown
+        }
+
+        if (timezone) {
+          date = timezone[0] === 'utc' ? date.utc(timezone[1]) : date.tz(timezone[0], timezone[1])
+        }
+
+        for (const [unit, value] of values) {
+          date = date.subtract(value, unit)
+        }
+
+        return date.toDate()
+      })
+    }
+
+    return schema
+  }
+}
